4910a02e259af93ad221518a3619de006434def2
MetehanOner <a.metehanoner@gmail.com>
2022-11-26 22:14:00 +0100

    test code 123

--- src/de/tum/in/ase/Stock.java
+++ src/de/tum/in/ase/Stock.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Objects;
 
 public class Stock {
-    //TODO: Implement Part 1
+    //TODO: Implement Part 1hhh
     private List<StockIngredient> stockIngredients;
 
     public Stock(){
