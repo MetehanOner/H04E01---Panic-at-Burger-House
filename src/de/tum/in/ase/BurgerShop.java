e72d2512a909ef543b033266e6335e74656475fa
MetehanOner <a.metehanoner@gmail.com>
2022-11-26 17:13:06 +0100

    third commit

--- src/de/tum/in/ase/BurgerShop.java
+++ src/de/tum/in/ase/BurgerShop.java
@@ -1,8 +1,10 @@
 package de.tum.in.ase;
 
+import java.util.List;
+
 public class BurgerShop {
     //TODO: Implement Part 1
-    public Burger orders;
+    public List<Burger> orders;
     private Stock stock;
 
     public int calculateTotalPrice(Burger burger){
@@ -19,6 +21,22 @@ public class BurgerShop {
 
     public void checkOrderReady(){
 
+    }
+
+    public void setOrders(List<Burger> orders) {
+        this.orders = orders;
+    }
+
+    public void setStock(Stock stock) {
+        this.stock = stock;
+    }
+
+    public List<Burger> getOrders() {
+        return orders;
+    }
+
+    public Stock getStock() {
+        return stock;
     }
 
     //TODO: Implement Part 3
--- src/de/tum/in/ase/Stock.java
+++ src/de/tum/in/ase/Stock.java
@@ -1,8 +1,10 @@
 package de.tum.in.ase;
 
+import java.util.List;
+
 public class Stock {
     //TODO: Implement Part 1
-    public StockIngredient stockIngredients;
+    public List<StockIngredient> stockIngredients;
 
     public int add(String a){
         return 0;
@@ -14,6 +16,14 @@ public class Stock {
     
     public StockIngredient findStockIngredient(String a){
         return new StockIngredient(a, 0,0);
+    }
+
+    public void setStockIngredients(List<StockIngredient> stockIngredients) {
+        this.stockIngredients = stockIngredients;
+    }
+
+    public List<StockIngredient> getStockIngredients() {
+        return stockIngredients;
     }
 
     //TODO: Implement Part 2
